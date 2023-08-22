0744878cf88dec5b833a314baeae8aa55f7664c2
sarardguezhe <sara.rodriguezhe@gmail.com>
2023-08-22 15:05:31 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$tweets = [
+    ["autor" => "Sara", "texto" => "mi primer tweet", "usuario" => "saraupgrade"],
+    ["autor" => "Carla", "texto" => "mi segundo tweet", "usuario" => "carlaupgrade"],
+    ["autor" => "Ana", "texto" => "mi tercer tweet", "usuario" => "anaupgrade"]
+];
+
+require_once "utils.php";
+require_once "index.html";
---
+++ utils.php
@@ -0,0 +1,40 @@
+<?php
+
+function getTweet(array $tweet): string{
+    $post = <<<TWEET
+    <div class="post">
+    <div class="post__avatar">
+      <img
+        src="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
+        alt=""
+      />
+    </div>
+
+    <div class="post__body">
+      <div class="post__header">
+        <div class="post__headerText">
+          <h3>
+            {$tweet["autor"]}
+            <span class="post__headerSpecial"
+              ><span class="material-icons post__badge"> verified </span>@{$tweet["usuario"]}</span>
+          </h3>
+        </div>
+        <div class="post__headerDescription">
+          <p>{$tweet["texto"]}</p>
+        </div>
+      </div>
+      <img
+        src="https://www.focus2move.com/wp-content/uploads/2020/01/Tesla-Roadster-2020-1024-03.jpg"
+        alt=""
+      />
+      <div class="post__footer">
+        <span class="material-icons"> repeat </span>
+        <span class="material-icons"> favorite_border </span>
+        <span class="material-icons"> publish </span>
+      </div>
+    </div>
+  </div>
+  TWEET;
+
+  return $post;
+}
